753c1ae60069e57fe0d7716c49d955ba02c2a408
GabrielBS46 <124404019+GabrielBS46@users.noreply.github.com>
2024-02-16 10:40:55 -0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 sudo apt install -y apache2
-sudo systemctl enable --now apache2
-
-sudo apt install mariadb-server
+sudo systemctl enable apache2
+sudo apt install -y mariadb-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-
-mysql_secure_installation <<EOF
-
+sudo mysql_secure_installation <<EOF
 y
-aSSASSIN-10
-aSSASSIN-10
+y
+password
+passeord
 y
 y
 y
 y
 EOF
-
-sudo apt install php libapache2-mod-php
+sudo apt install -y php libapache2-mod-php
