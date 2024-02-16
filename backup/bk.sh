a5a66a699948b399518849553cdab717ea766f82
GabrielBS46 <gabriel.bitencourt46@gmail.com>
2024-02-16 18:13:27 -0300

    path

---
+++ backup/bk.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+#
+backup(){
+  now=$(date +%Y%m%d)
+  bkdir=backup/$now
+  secret=$Ac0m1P99
+
+  mkdir -p $bkdir
+
+  echo Backup sendo feito $(date '+%d/%m/%y - %H:%M:%S')
+  #echo Mysql senha requerida...
+  cp -r /var/www/html $bkdir
+  cp -r /etc/asterisk $bkdir
+  mysqldump -u root -p=$secret asterisk > $bkdir/asterisk.sql
+  mysqldump -u root -p=$secret web > $bkdir/web.sql
+
+  tar cjf backup$now.tar.bz2 $bkdir
+}
+
+
+backup
