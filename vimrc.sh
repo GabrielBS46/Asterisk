d4c9c10ee695a0ad277b882aec94a8939ae213a2
GabrielBS46 <124404019+GabrielBS46@users.noreply.github.com>
2024-02-16 10:26:21 -0300

    Update vimrc.sh
    Sintaxe line3 and 8

--- vimrc.sh
+++ vimrc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "alias ll='ls -l --color'" >> ~/.bashrc
-echo "set tabstop=2 softtabstop=2 shifwidth=2" >> ~/.vimrc
+echo "set tabstop=2 softtabstop=2 shiftwidth=2" >> ~/.vimrc
 echo "set expandtab" >> ~/.vimrc
 echo "set number ruler" >> ~/.vimrc
 echo "set autoindent smartindent" >> ~/.vimrc
 echo "syntax enable" >> ~/.vimrc
-echo "Filetype plugin indent on" >> ~/.vimrc
+echo "filetype plugin indent on" >> ~/.vimrc
