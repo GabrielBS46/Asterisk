005d428370f21834935a641f5683c525d6117792
GabrielBS46 <124404019+GabrielBS46@users.noreply.github.com>
2024-02-16 18:25:33 -0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+  echo Forneça a senha junto ao comando. 
+  echo ./install.sh <password>
+  exit 2
+fi
+
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y git vim nano wget rsync unzip curl
-
 bash vimrc.sh
 bash lamp.sh $1
 bash asterisk.sh
