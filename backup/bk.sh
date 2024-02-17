b61704b428483e762f750c99370748b6ed1a9fe6
GabrielBS46 <gabriel.bitencourt46@gmail.com>
2024-02-17 18:18:52 -0300

    Update bk.sh
    Acrescentando opções

--- backup/bk.sh
+++ backup/bk.sh
@@ -1,13 +1,13 @@
 #/bin/bash
-#
+# Script para bk
+
 backup(){
   now=$(date +%Y%m%d)
   bkdir=backup/$now
-  secret=$1
 
   mkdir -p $bkdir
 
-  echo Backup sendo feito $(date '+%d/%m/%y - %H:%M:%S')
+  printf "\nBackup sendo feito $(date '+%d/%m/%y - %H:%M:%S')\n"
   #echo Mysql senha requerida...
   cp -r /var/www/html $bkdir
   cp -r /etc/asterisk $bkdir
@@ -17,12 +17,28 @@ backup(){
   tar cjf backup$now.tar.bz2 $bkdir
 }
 
-
 if [ -z "$1" ]
 then
-  echo Forneça a senha junto ao comando. 
-  echo "./bk.sh <password>"
+  echo Selecione a opção
+  echo "./bk.sh -h para mais informações"
   exit 2
 fi
 
-backup
+case $1 in
+  -i)
+    read -s -p "Entre com sua senha do Banco de dados: " secret
+    backup
+  ;;
+  -r)
+   echo ainda em testes
+  ;;
+  -h | --help)
+    echo './bk.sh <opcão> <arquivo>'
+    echo './bk.sh -h --help    Mostra esse prompt de help'
+    echo './bk.sh -i           Inicia um novo Backup do sistema e banco'
+    echo './bk.sh -r <arquivo> Restabelece um backup'
+    exit 0
+  ;;
+
+esac
+
