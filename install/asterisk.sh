17bcb2bf5ebf96cfcb1fd6f2c91bea86f9f288c3
GabrielBS46 <124404019+GabrielBS46@users.noreply.github.com>
2024-03-11 10:55:34 -0300

    Atualizar o asterisk.sh

--- install/asterisk.sh
+++ install/asterisk.sh
@@ -8,6 +8,6 @@ cd asterisk*
 sudo ./contrib/scripts/install_prereq install
 sudo ./configure && sudo make && sudo make install && sudo make config && sudo make install-logrotate
 sudo systemctl start asterisk.service
-sudo systemctl enabled asterisk.service
+sudo systemctl enable asterisk.service
 sudo useradd asterisk
 sudo chown -R asterisk: {/etc,/var/lib,/var/spool,/var/log,/var/run}/asterisk
